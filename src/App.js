9e74de281e41f7e12df955664cc44d70f72af4d1
AlexTicovschi <alexmticovschi@gmail.com>
2020-07-07 17:47:31 +0100

    creating the store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React from 'react';
 // components
-import Navbar from "./components/Navbar";
-import CartContainer from "./components/CartContainer";
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
 // items
-import cartItems from "./cart-items";
-// redux stuff
+import cartItems from './cart-items';
+// redux
+import { createStore } from 'redux';
+
+// reducer
+const reducer = (state, action) => {
+  console.log({ state, action });
+};
+
+const store = createStore(reducer);
 
 function App() {
   // cart setup
