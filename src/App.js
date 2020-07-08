48506472c358a79ddb6fa62c9ab1343f9d0e5753
AlexTicovschi <alexmticovschi@gmail.com>
2020-07-08 08:27:53 +0100

    connecting the CartContainer to Redux store

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ import { Provider } from 'react-redux';
 
 const initialState = {
   cart: cartItems,
-  total: 0,
-  amount: 0,
+  total: 105,
+  amount: 5,
 };
 
 const store = createStore(reducer, initialState);
@@ -21,7 +21,7 @@ function App() {
   return (
     <Provider store={store}>
       <Navbar />
-      <CartContainer cart={cartItems} />
+      <CartContainer />
     </Provider>
   );
 }
--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,28 +1,28 @@
-import React from "react";
+import React from 'react';
 
 const CartItem = ({ img, title, price, amount }) => {
   return (
-    <div className="cart-item">
+    <div className='cart-item'>
       <img src={img} alt={title} />
       <div>
         <h4>{title}</h4>
-        <h4 className="item-price">${price}</h4>
+        <h4 className='item-price'>&pound;{price}</h4>
         {/* remove button */}
-        <button className="remove-btn">remove</button>
+        <button className='remove-btn'>remove</button>
       </div>
       <div>
         {/* increase amount */}
-        <button className="amount-btn">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-            <path d="M10.707 7.05L10 6.343 4.343 12l1.414 1.414L10 9.172l4.243 4.242L15.657 12z" />
+        <button className='amount-btn'>
+          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
+            <path d='M10.707 7.05L10 6.343 4.343 12l1.414 1.414L10 9.172l4.243 4.242L15.657 12z' />
           </svg>
         </button>
         {/* amount */}
-        <p className="amount">{amount}</p>
+        <p className='amount'>{amount}</p>
         {/* decrease amount */}
-        <button className="amount-btn">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+        <button className='amount-btn'>
+          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
+            <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
           </svg>
         </button>
       </div>
